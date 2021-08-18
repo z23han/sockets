e685af601bc713303ce8261a52d2c8a3fe59c710
z23han <zxhan2010@hotmail.com>
2021-08-18 17:19:02 -0400

    init commit

---
+++ http_server.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+int main() {
+
+    // open a file to serve
+    FILE *html_data;
+
+    html_data = fopen("index.html", "r");
+
+    char response_data[1024];
+    
+    fgets(response_data, 1024, html_data);
+
+    char http_header[2048] = "HTTP/1.1 200 OK\r\n\r\n";
+
+    strcat(http_header, response_data);
+
+    // create a socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // define the address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8001);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // bind the socket to the call
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    listen(server_socket, 5);
+
+    int client_socket;
+
+    for (;;) {
+        client_socket = accept(server_socket, NULL, NULL);
+        send(client_socket, http_header, strlen(http_header), 0);
+        close(client_socket);
+    }
+
+    return 0;
+}
